ceed65f4b3ba25320b23a27f86a54838232b18e2
gigormo <54334852+gigormo@users.noreply.github.com>
2026-01-09 17:42:34 -0600

    Comment out bundle move logic and build img

--- Assets/Editor/CreateAssetBundle.cs
+++ Assets/Editor/CreateAssetBundle.cs
@@ -1,29 +1,28 @@
 using System.IO;
 using UnityEditor;
-using UnityEngine;
 
 #if UNITY_EDITOR
 
 public class CreateAssetBundles {
+
     [MenuItem("Assets/Build AssetBundles")]
     private static void BuildAllAssetBundles() {
         string assetBundleDirectory = "Assets/AssetBundles";
-        string destinationPath = "E:/Games/Stickman Strikes/BepInEx/plugins/KMod/kmodbundle";
+        //string destinationPath = "E:/Games/Stickman Strikes/BepInEx/plugins/KMod/kmodbundle";
 
-        Debug.Log($"GetAccessControl: {destinationPath}");
+        //Debug.Log($"GetAccessControl: {destinationPath}");
 
         // 1. Ensure local folder exists for the build process
         if (!Directory.Exists(assetBundleDirectory)) _ = Directory.CreateDirectory(assetBundleDirectory);
 
         _ = BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
 
-        Debug.Log($"{File.Exists($"{assetBundleDirectory}/kmodbun")}");
-
-        if (File.Exists($"{assetBundleDirectory}/kmodbundle")) {
-            if (File.Exists(destinationPath)) File.Delete(destinationPath);
-            File.Move($"{assetBundleDirectory}/kmodbundle", destinationPath);
-            Debug.Log("Bundle moved successfully!");
-        }
+        //if (File.Exists($"{assetBundleDirectory}/kmodbundle")) {
+        //    if (File.Exists(destinationPath)) File.Delete(destinationPath);
+        //    File.Move($"{assetBundleDirectory}/kmodbundle", destinationPath);
+        //    Debug.Log("Bundle moved successfully!");
+        //}
     }
 }
+
 #endif
